ae546f6932cf30b5334aab3e1c8881915f152ace
Dredrepug <31702344+Dredrepug@users.noreply.github.com>
2017-10-04 16:55:34 -0700

    Saved Boost Script

--- Assets/Scripts/Boost.cs
+++ Assets/Scripts/Boost.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boost : MonoBehaviour {
+
     public bool boostActive;
-
-
     public Animator myAnim;
 
 	// Use this for initialization
